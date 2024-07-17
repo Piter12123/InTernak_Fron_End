1fb5a3a67064ad3b3bc2558d327d4c8bb8bfd3d7
piter12123 <petersinisia@gmail.com>
2024-07-17 16:10:25 +0700

    Nampil Notifikasi

--- app/src/main/java/com/polytechnic/astra/ac/id/internak/Fragment/EditKatasandiFragment.java
+++ app/src/main/java/com/polytechnic/astra/ac/id/internak/Fragment/EditKatasandiFragment.java
@@ -1,66 +1,123 @@
 package com.polytechnic.astra.ac.id.internak.Fragment;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.gson.Gson;
+import com.polytechnic.astra.ac.id.internak.Model.UserModel;
 import com.polytechnic.astra.ac.id.internak.R;
+import com.polytechnic.astra.ac.id.internak.ViewModel.UserViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditKatasandiFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EditKatasandiFragment extends Fragment {
+    private static final String TAG = "EditKatasandiFragment";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText oldPasswordEditText;
+    private EditText newPasswordEditText;
+    private EditText confirmPasswordEditText;
+    private ImageButton toggleOldPasswordButton;
+    private ImageButton toggleNewPasswordButton;
+    private ImageButton toggleConfirmPasswordButton;
+    private Button saveButton;
+    private boolean isOldPasswordVisible = false;
+    private boolean isNewPasswordVisible = false;
+    private boolean isConfirmPasswordVisible = false;
+    private UserViewModel userViewModel;
 
     public EditKatasandiFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditKatasandiFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditKatasandiFragment newInstance(String param1, String param2) {
-        EditKatasandiFragment fragment = new EditKatasandiFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_katasandi, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_katasandi, container, false);
+
+        oldPasswordEditText = view.findViewById(R.id.sandilama);
+        newPasswordEditText = view.findViewById(R.id.sandibaru);
+        confirmPasswordEditText = view.findViewById(R.id.konfirmasi);
+        saveButton = view.findViewById(R.id.save_button);
+        toggleOldPasswordButton = view.findViewById(R.id.sandilamaview);
+        toggleNewPasswordButton = view.findViewById(R.id.sandibaruview);
+        toggleConfirmPasswordButton = view.findViewById(R.id.konfirmasiview);
+
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+        toggleOldPasswordButton.setOnClickListener(v -> togglePasswordVisibility(oldPasswordEditText, toggleOldPasswordButton));
+        toggleNewPasswordButton.setOnClickListener(v -> togglePasswordVisibility(newPasswordEditText, toggleNewPasswordButton));
+        toggleConfirmPasswordButton.setOnClickListener(v -> togglePasswordVisibility(confirmPasswordEditText, toggleConfirmPasswordButton));
+
+        saveButton.setOnClickListener(v -> {
+            String oldPassword = oldPasswordEditText.getText().toString().trim();
+            String newPassword = newPasswordEditText.getText().toString().trim();
+            String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+
+            if (TextUtils.isEmpty(oldPassword) || TextUtils.isEmpty(newPassword) || TextUtils.isEmpty(confirmPassword)) {
+                Toast.makeText(getActivity(), "Semua kolom wajib diisi", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!newPassword.equals(confirmPassword)) {
+                Toast.makeText(getActivity(), "Kata sandi baru dan konfirmasi kata sandi tidak sama", Toast.LENGTH_SHORT).show();
+                Clear();
+                return;
+            }
+
+            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("loginSession", Context.MODE_PRIVATE);
+            String userJson = sharedPreferences.getString("dataUser", null);
+
+            if (userJson != null) {
+                UserModel userModel = new Gson().fromJson(userJson, UserModel.class);
+                if (oldPassword.equals(userModel.getUsrPassword())) {
+                    userViewModel.changePassword(oldPassword, newPassword);
+                    Toast.makeText(getActivity(), "Kata sandi berhasil diubah", Toast.LENGTH_SHORT).show();
+                    Clear();
+                    navigateToNextActivity();
+                } else {
+                    Toast.makeText(getActivity(), "Kata sandi lama salah", Toast.LENGTH_SHORT).show();
+                    Clear();
+                }
+            }
+        });
+
+        return view;
+    }
+    private void togglePasswordVisibility(EditText passwordEditText, ImageButton toggleButton) {
+        if (passwordEditText.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            toggleButton.setImageResource(R.drawable.icon_mata);
+        } else {
+            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            toggleButton.setImageResource(R.drawable.icon_mata);
+        }
+        passwordEditText.setSelection(passwordEditText.length());
+    }
+    private void Clear(){
+        oldPasswordEditText.setText("");
+        newPasswordEditText.setText("");
+        confirmPasswordEditText.setText("");
+    }
+    private void navigateToNextActivity() {
+        try {
+            UserFragment userFragment = new UserFragment();
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_login, userFragment)
+                    .addToBackStack(null)
+                    .commit();
+        } catch (Exception e) {
+            Log.e(TAG, "Error starting UserFragment", e);
+        }
     }
 }
